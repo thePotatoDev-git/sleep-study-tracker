d0fccb5381ac79b3afeb06f47b7da680e5d9889c
Albertus Chua <albertus.chua@gmail.com>
2022-07-25 16:15:14 -0400

    Updated naming conventions to camelCase

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function openLab (event, labName) {
-    let tabcontent = document.getElementsByClassName('tabcontent');
-    let tablinks = document.getElementsByClassName('tablinks');
+    let tabContent = document.getElementsByClassName('tabContent');
+    let tabLinks = document.getElementsByClassName('tabLinks');
 
-    for (let i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
+    for (let i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = 'none';
     }
 
-    for (let i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(' active', '');
+    for (let i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(' active', '');
     }
 
     document.getElementById(labName).style.display = 'block';
