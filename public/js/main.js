1fe41dff6b242a9588e81d3cd2b254d50388526b
Albertus Chua <albertus.chua@gmail.com>
2022-07-27 19:53:59 -0400

    Added deleteStudy function

--- public/js/main.js
+++ public/js/main.js
@@ -21,3 +21,32 @@ function openForm() {
 function closeForm() {
     document.getElementById('patient-form').style.display = 'none';
 }
+
+const deleteRow = document.querySelectorAll('.fa-trash-can');
+const editRow = document.querySelectorAll('.fa-pen-to-square');
+
+Array.from(deleteRow).forEach(element => {
+    element.addEventListener('click', deleteStudy)
+});
+
+async function deleteStudy() {
+    const lName = this.parentNode.childNodes[3].innerText;
+    const fName = this.parentNode.childNodes[5].innerText;
+    const sDate = this.parentNode.childNodes[7].innerText;
+    try {
+        const response = await fetch('deleteStudy', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'lastNameS': lName,
+                'firstNameS': fName,
+                'studyDateS': sDate
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
