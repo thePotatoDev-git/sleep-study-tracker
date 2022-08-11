47e49b106ae7a7eae68e82f32f49049212ffae7d
Albertus Chua <albertus.chua@gmail.com>
2022-08-11 12:54:44 -0400

    Removed function for opening lab tables with tabs on single page

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,18 @@
-function openLab (event, labName) {
-    let tabContent = document.getElementsByClassName('tab-content');
-    let tabLinks = document.getElementsByClassName('tab-links');
+// function openLab (event, labName) {
+//     let tabContent = document.getElementsByClassName('tab-content');
+//     let tabLinks = document.getElementsByClassName('tab-links');
 
-    for (let i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = 'none';
-    }
+//     for (let i = 0; i < tabContent.length; i++) {
+//         tabContent[i].style.display = 'none';
+//     }
 
-    for (let i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(' active', '');
-    }
+//     for (let i = 0; i < tabLinks.length; i++) {
+//         tabLinks[i].className = tabLinks[i].className.replace(' active', '');
+//     }
 
-    document.getElementById(labName).style.display = 'block';
-    event.currentTarget.className += ' active';
-}
+//     document.getElementById(labName).style.display = 'block';
+//     event.currentTarget.className += ' active';
+// }
 
 function openForm() {
     document.getElementById('patient-form').style.display = 'block';
@@ -31,11 +31,11 @@ Array.from(deleteRow).forEach(element => {
 
 async function deleteStudy() {
     console.log(document.querySelectorAll('.first-name')[0].innerText);
-    console.log(document.querySelector('.last-name'));
-    console.log(document.querySelector('.study-date'));
-    // const lName = document.querySelectorAll('.last-name')[0].innerText;
-    // const fName = document.querySelectorAll('.first-name')[0].innerText;
-    // const sDate = document.querySelectorAll('.study-date')[0].innerText;
+    console.log(document.querySelectorAll('.last-name')[0].innerText);
+    console.log(document.querySelectorAll('.study-date')[0].innerText);
+    const lName = document.querySelectorAll('.last-name')[0].innerText;
+    const fName = document.querySelectorAll('.first-name')[0].innerText;
+    const sDate = document.querySelectorAll('.study-date')[0].innerText;
     try {
         const response = await fetch('deleteStudy', {
             method: 'delete',
