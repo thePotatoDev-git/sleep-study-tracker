59958aaad6902be1200ea23b1b1183ac86de309a
Albertus Chua <albertus.chua@gmail.com>
2022-10-06 20:28:04 -0400

    Updated package.json for Cyclic launch

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.use('/study', studiesRoutes);
 app.use('/edit', editRoutes);
 
 // Port listening
-app.listen(process.env.PORT || 3000, () =>{
+app.listen(process.env.PORT, () =>{
     console.log(`Server running on ${process.env.PORT}`)
 });
